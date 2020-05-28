987023674e2ffd3683021693331e1b260d23f43f
experion-tibin <tibin.thomas@experionglobal.com>
2020-05-28 17:03:00 +0530

    updatedFSCalendarMaximumEventDotDiameter

--- FSCalendar/FSCalendarConstants.m
+++ FSCalendar/FSCalendarConstants.m
@@ -23,7 +23,7 @@ CGFloat const FSCalendarStandardTitleTextSize = 13.5;
 CGFloat const FSCalendarStandardSubtitleTextSize = 10;
 CGFloat const FSCalendarStandardWeekdayTextSize = 14;
 CGFloat const FSCalendarStandardHeaderTextSize = 16.5;
-CGFloat const FSCalendarMaximumEventDotDiameter = 4.5;
+CGFloat const FSCalendarMaximumEventDotDiameter = 4.0;
 
 NSInteger const FSCalendarDefaultHourComponent = 0;
 NSInteger const FSCalendarMaximumNumberOfEvents = 4;
